7b998dfbdbdbf4dc8b623d1ea83a7d429dab7a5a
SimonGreyHull <S.Grey@hull.ac.uk>
2020-11-21 12:30:29 +0000

    Takeaway with loops

--- DemoConsoleApplication/Program.cs
+++ DemoConsoleApplication/Program.cs
@@ -6,20 +6,112 @@ namespace DemoConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a number");
+            Console.WriteLine("Welcome to my takeaway");
+            int costInPence = 0;
 
-            string input = Console.ReadLine();
+            string inputString;
+            int selection;
 
-            int number = int.Parse(input);
-
-            if(number >= 100)
+            do
             {
-                Console.WriteLine("That's a big number");
+                do
+                {
+                    Console.WriteLine("What would you like to eat?");
+                    Console.WriteLine("1. Burger");
+                    Console.WriteLine("2. Pizza");
+
+                    inputString = Console.ReadLine();
+                    selection = int.Parse(inputString);
+                } while (!(selection == 1 || selection == 2));
+
+                if (selection == 1)
+                {
+                    do
+                    {
+                        Console.WriteLine("Quarter Pound or Half Pound?");
+                        Console.WriteLine("1. 1/4 lb.");
+                        Console.WriteLine("2. 1/2 lb.");
+                        inputString = Console.ReadLine();
+                        selection = int.Parse(inputString);
+                    } while (! (selection == 1 || selection == 2)) ;
+                    
+                    if (selection == 1)
+                    {
+                        costInPence += 250;
+                    }
+                    else if (selection == 2)
+                    {
+                        costInPence += 350;
+                    }
+
+                    do
+                    {
+                        Console.WriteLine("Would you like cheese?");
+                        Console.WriteLine("Enter y for yes or n for no");
+                        inputString = Console.ReadLine();
+                    } while (!(inputString == "y" || inputString == "n"));
+
+                    if (inputString == "y")
+                    {
+                        costInPence += 100;
+                    }
+                }
+                else if (selection == 2)
+                {
+                    do
+                    {
+                        Console.WriteLine("What size?");
+                        Console.WriteLine("1. Medium");
+                        Console.WriteLine("2. Large");
+                        Console.WriteLine("3. XL");
+                        inputString = Console.ReadLine();
+                        selection = int.Parse(inputString);
+
+                        if (selection == 1)
+                        {
+                            costInPence += 500;
+                        }
+                        else if (selection == 2)
+                        {
+                            costInPence += 700;
+                        }
+                        else if (selection == 3)
+                        {
+                            costInPence += 900;
+                        }
+                    } while (selection < 1 || selection > 3);
+                }
+
+                inputString = "";
+
+                while (!(inputString == "y" || inputString == "n"))
+                {
+
+                    Console.WriteLine("Current order total is " + costInPence + " pence.");
+                    Console.WriteLine("Would you like anything else?");
+                    Console.WriteLine("Enter y for yes or n for no");
+
+                    inputString = Console.ReadLine();
+
+                };
+
+            } while (inputString == "y");
+
+            if(costInPence > 600)
+            {
+                Console.WriteLine("Have a free garlic bread!");
+            }
+
+            if(costInPence > 1000)
+            {
+                Console.WriteLine("Free Delivery");
             }
             else
             {
-                Console.WriteLine("That's a small number");
+                costInPence += 200;
             }
+
+            Console.WriteLine("Cost is " + costInPence); 
         }
     }
 }
