bd270d412de7392d4a1e31105a46488d9601072a
SimonGreyHull <S.Grey@hull.ac.uk>
2020-11-21 13:03:42 +0000

    Simons High Score Game - Break and Continue

--- DemoConsoleApplication/Program.cs
+++ DemoConsoleApplication/Program.cs
@@ -6,112 +6,45 @@ namespace DemoConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to my takeaway");
-            int costInPence = 0;
-
-            string inputString;
-            int selection;
+            Console.WriteLine("Simon's High Score Game!");
+            int highscore = 0;
 
             do
             {
-                do
+                Console.WriteLine("What score would you like?");
+                string inputString = Console.ReadLine();
+                int score = int.Parse(inputString);
+
+                if(score > highscore)
                 {
-                    Console.WriteLine("What would you like to eat?");
-                    Console.WriteLine("1. Burger");
-                    Console.WriteLine("2. Pizza");
-
-                    inputString = Console.ReadLine();
-                    selection = int.Parse(inputString);
-                } while (!(selection == 1 || selection == 2));
-
-                if (selection == 1)
-                {
-                    do
-                    {
-                        Console.WriteLine("Quarter Pound or Half Pound?");
-                        Console.WriteLine("1. 1/4 lb.");
-                        Console.WriteLine("2. 1/2 lb.");
-                        inputString = Console.ReadLine();
-                        selection = int.Parse(inputString);
-                    } while (! (selection == 1 || selection == 2)) ;
-                    
-                    if (selection == 1)
-                    {
-                        costInPence += 250;
-                    }
-                    else if (selection == 2)
-                    {
-                        costInPence += 350;
-                    }
-
-                    do
-                    {
-                        Console.WriteLine("Would you like cheese?");
-                        Console.WriteLine("Enter y for yes or n for no");
-                        inputString = Console.ReadLine();
-                    } while (!(inputString == "y" || inputString == "n"));
-
-                    if (inputString == "y")
-                    {
-                        costInPence += 100;
-                    }
-                }
-                else if (selection == 2)
-                {
-                    do
-                    {
-                        Console.WriteLine("What size?");
-                        Console.WriteLine("1. Medium");
-                        Console.WriteLine("2. Large");
-                        Console.WriteLine("3. XL");
-                        inputString = Console.ReadLine();
-                        selection = int.Parse(inputString);
-
-                        if (selection == 1)
-                        {
-                            costInPence += 500;
-                        }
-                        else if (selection == 2)
-                        {
-                            costInPence += 700;
-                        }
-                        else if (selection == 3)
-                        {
-                            costInPence += 900;
-                        }
-                    } while (selection < 1 || selection > 3);
+                    Console.WriteLine("Congratulations! " + score + " is the high score");
+                    highscore = score;
                 }
 
-                inputString = "";
+                Console.WriteLine("Would you like to play again?");
 
-                while (!(inputString == "y" || inputString == "n"))
+                inputString = Console.ReadLine();
+
+                if(inputString != "no")
                 {
+                    continue;
+                }
 
-                    Console.WriteLine("Current order total is " + costInPence + " pence.");
-                    Console.WriteLine("Would you like anything else?");
-                    Console.WriteLine("Enter y for yes or n for no");
+                Console.WriteLine("Are you sure you want to quit?");
 
-                    inputString = Console.ReadLine();
+                inputString = Console.ReadLine();
 
-                };
+                if(inputString == "yes")
+                {
+                    Console.WriteLine("Goodbye. Come back soon!");
+                    break;
+                }
 
-            } while (inputString == "y");
+                Console.WriteLine("Great, Let's play again then!");
 
-            if(costInPence > 600)
-            {
-                Console.WriteLine("Have a free garlic bread!");
-            }
+            } while (true);
+            Console.WriteLine("Program will now exit");
 
-            if(costInPence > 1000)
-            {
-                Console.WriteLine("Free Delivery");
-            }
-            else
-            {
-                costInPence += 200;
-            }
-
-            Console.WriteLine("Cost is " + costInPence); 
         }
     }
 }
